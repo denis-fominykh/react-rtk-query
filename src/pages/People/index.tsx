984a27a19033418684731e1bf8e126afac7e2d11
denis_fominykh <dfominykh@s-pro.io>
2022-08-02 17:07:57 +0300

    feat: create people page

---
+++ src/pages/People/index.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+
+import { Loader } from 'components/Loader';
+import { ItemsList, PageHeader } from 'pages/People/styled';
+import { useGetAllPeopleQuery } from 'service/peopleApi';
+
+export function People(): JSX.Element {
+  const { isLoading, isError, data, status } = useGetAllPeopleQuery();
+
+  return (
+    <>
+      <PageHeader>
+        <h1>People</h1>
+      </PageHeader>
+      <ItemsList>
+        {isLoading ? (
+          <Loader />
+        ) : isError ? (
+          <h1>{status}</h1>
+        ) : (
+          data?.results.map(({ name, birth_year: birthYear }, idx) => (
+            <Link key={idx} to={`/people/${idx + 1}`}>
+              <li>
+                <h3>{name}</h3>
+                Birth year: {birthYear}
+              </li>
+            </Link>
+          ))
+        )}
+      </ItemsList>
+    </>
+  );
+}
